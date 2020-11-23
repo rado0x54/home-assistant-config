f1d123d15cb74c551d971f50a6ba1d49367c7722
Martin Riedel <web@riedel-privat.de>
2020-11-23 16:28:55 -0500

    Replaced carson code with submodule integration

--- custom_components/carson/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Carson integration."""
-
-DOMAIN = "carson"
-UNLOCKED_TIMESPAN_SEC = 5
-ATTRIBUTION = "provided by Eagle Eye"
-
-CONF_LIST_FROM_EAGLE_EYE = "list_from_eagle_eye"
-DEFAULT_CONF_LIST_FROM_EAGLE_EYE = False
